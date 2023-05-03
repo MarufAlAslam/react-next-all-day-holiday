538fa1c5e2d376899528c1565198a32eb178e5ad
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-05-03 14:53:25 +0600

    banner done, stoped for today

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Hero from '../components/hero/Hero'
+// import Carousel from '../components/slider/Slider'
+
+
 export default function Home() {
   return (
+    <>
     <Hero/>
+    {/* <Carousel images={undefined}/> */}
+    </>
   )
 }
