1165f665114428c9f2e70b35dc6bd018e4058ede
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-05-03 11:08:21 +0600

    ui library initiated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '../assets/styles/style.css'
+
 import Main from "../components/layout/Main"
 
 export default function App({ Component, pageProps }: AppProps) {
