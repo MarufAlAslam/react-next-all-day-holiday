731db0552da4f922bca09b6e8630535bf5f231c4
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-05-08 22:06:59 +0600

    input style changed

--- components/newsLeter/NewsLetter.js
+++ components/newsLeter/NewsLetter.js
@@ -20,7 +20,7 @@ otrzymywać regularne wiadomości, aktualizacje i oferty specjalne.
 
                 <form className='mt-3 lg:w-1/3'>
                     <div className='flex w-full p-1 rounded-full bg-white flex-row justify-center items-center'>
-                        <input type='email' placeholder='Twój adres e-mail' className='w-full text-[15px] h-12 rounded-lg border-2 border-white px-4'/>
+                        <input type='email' placeholder='Twój adres e-mail' className='w-full text-[15px] h-12 rounded-full bg-white border-2 border-white px-4'/>
                         <button className='btn rounded-full btn-golden'>Subskrybuj</button>
                     </div>
                 </form>
